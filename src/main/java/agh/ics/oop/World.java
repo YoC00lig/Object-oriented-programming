75494c45ecc252105a68150451855f68c32ecdc1
YoC00lig <kulig.j@vp.pl>
2022-10-08 00:53:10 +0200

    first lab

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,24 +2,34 @@ package agh.ics.oop;
 
 public class World {
     public static void main(String[] args){
-        System.out.println("System wystartował.");
-        run(args);
-        System.out.println("System zakończył działanie.");
-    }
-
-    public static void run(String[] array){
-        System.out.println("Zwierzak idzie do przodu.");
-        int cnt = 0;
-        for(String argument:array){
-            cnt++;
-            if (cnt == array.length){
-                System.out.print(argument);
-            } else {
-                System.out.print(argument + ", ");
-            }
+        System.out.println("Start.");
+        Direction[] dirs = new Direction[args.length];
+        for (int i = 0; i < args.length; i++){
+            String move = args[i];
+            dirs[i] = switch (move){
+                case "f" -> Direction.FORWARD;
+                case "b" -> Direction.BACKWARD;
+                case "l" -> Direction.LEFT;
+                case "r" -> Direction.RIGHT;
+                default -> Direction.UNDEFINED;
+            };
         }
-        System.out.println();
+        run(dirs);
+        System.out.println("Stop.");
     }
 
+    public static void run(Direction[] directions){
+        for(Direction move: directions){
+            String answer = switch (move){
+                case FORWARD -> "Zwierzak idzie do przodu.";
+                case BACKWARD -> "Zwierzak idzie do tyłu.";
+                case LEFT -> "Zwierzak skręca w lewo.";
+                case RIGHT -> "Zwierzak skręca w prawo.";
+                case UNDEFINED -> null;
+            };
+            if (answer != null) {System.out.println(answer);}
+            }
+    }
 }
+
 
